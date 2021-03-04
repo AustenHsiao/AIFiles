41aab71234e0161df4581b1df97884ae33cfc791
AustenHsiao <Austenhsiao@gmail.com>
2021-03-03 20:50:06 -0800

    something is not right

--- QLearningRobby.py
+++ QLearningRobby.py
@@ -1,107 +1,134 @@
-# Robby the Robot using Q-learning
+# Robby the Robot using Q-learning. Written by Austen Hsiao for AI at PSU
 import random as r
 import numpy as np
 import matplotlib.pyplot as plt
 
+r.seed()
 
-class board:
-    # define a 10x10 grid with 0=clean 1=can
+
+class Robby:
     def __init__(self):
-        self.position = (r.randint(0, 9), r.randint(0, 9))
         self.board = [[r.randint(0, 1) for i in range(10)] for ii in range(10)]
+        self.currentLocation = (r.randint(0, 9), r.randint(0, 9))
 
-    # returns a 5-tuple containing the rewards from performing the actions (up, down, left, right, pick up)
-    # does not actually make a move
-    def observe(self):
-        x = self.position[0]
-        y = self.position[1]
+    def checkSensor(self):
+        x = self.currentLocation[0]
+        y = self.currentLocation[1]
+        current = (x, y)
+        north = (x-1, y)
+        south = (x+1, y)
+        east = (x, y+1)
+        west = (x, y-1)
+        return "{c} {n} {s} {e} {w}".format(c=self.tileCheck(current), n=self.tileCheck(north), s=self.tileCheck(south), e=self.tileCheck(east), w=self.tileCheck(west))
 
-        up, down, left, right = 0, 0, 0, 0
-        if x - 1 < 0:
-            up = -5
-        if x + 1 > 9:
-            down = -5
-        if y - 1 < 0:
-            left = -5
-        if y + 1 > 9:
-            right = -5
-        if self.board[x][y]:
-            return (up, down, left, right, 10)
-        return (up, down, left, right, -1)
+    # used internally
+    def tileCheck(self, tile):
+        if tile[0] < 0 or tile[0] > 9 or tile[1] < 0 or tile[1] > 9:
+            return 'W'  # Wall
+        elif self.board[tile[0]][tile[1]] == 1:
+            return 'C'  # Can
+        return 'E'  # Empty
 
-    # m is 0:4, corresponding to each of the actions, (up, down, left, right, pick up)
-    # updates the position
-    def move(self, m):
-        x = self.position[0]
-        y = self.position[1]
-        new_x = x
-        new_y = y
-
+    def performAction(self, m):
+        x = self.currentLocation[0]
+        y = self.currentLocation[1]
+        reward = 0
+        moveTo_x = x
+        moveTo_y = y
+        # pick up can
         if m == 0:
-            new_x -= 1
-        elif m == 1:
-            new_x += 1
-        elif m == 2:
-            new_y -= 1
-        elif m == 3:
-            new_y += 1
+            if self.board[x][y] == 1:
+                self.board[x][y] = 0
+                return 10
+            else:
+                return -1
+        elif m == 1:  # north
+            moveTo_x -= 1
+        elif m == 2:  # south
+            moveTo_x += 1
+        elif m == 3:  # east
+            moveTo_y += 1
+        elif m == 4:  # west
+            moveTo_y -= 1
 
-        if new_x < 0 or new_x > 9:
-            new_x = x
-        if new_y < 0 or new_y > 9:
-            new_y = y
+        if moveTo_x < 0 or moveTo_x > 9:  # Wall
+            moveTo_x = x
+            reward = -5
+        elif moveTo_y < 0 or moveTo_y > 9:
+            moveTo_y = y
+            reward = -5
+        self.currentLocation = (moveTo_x, moveTo_y)
+        return reward
 
-        self.position = (new_x, new_y)
-        return
 
-
-class q_learn:
-    # 5 actions (up(0), down(1), left(2), right(3), pick up a can(4))
-    # 100 states-- initialized to 0
+class QLearn:
     def __init__(self):
-        self.qTable = [[0 for i in range(5)] for ii in range(100)]
+        possibleState = ['W', 'C', 'E']
+        self.qTable = {}
+        for i in possibleState:
+            for ii in possibleState:
+                for iii in possibleState:
+                    for iv in possibleState:
+                        for v in possibleState:
+                            self.qTable["{current} {north} {south} {east} {west}".format(
+                                current=i, north=ii, south=iii, east=iv, west=v)] = [0, 0, 0, 0, 0]
+        # This is ugly but it will make the logic very simple. The states are stored as key:value pairs:
+        # "currentState northState southState eastState westState":[current, north, south, east, west]
         self.episodes = 5000
         self.steps = 200
         self.learningRate = 0.2
         self.discountFactor = 0.9
-        self.epsilon = 0.1  # decreases every 50 epochs until 0
 
-    def greedyMove(self, board):
-        # determine which action to take
-        observed_rewards = np.array(board.observe())
-        primary_move = np.random.choice(np.flatnonzero(
-            observed_rewards == observed_rewards.max()))  # random tiebreaking for maximum value
+    def greedyMove(self, grid, epsilon):
+        # 1 observe current state.
+        # 2 choose action
+        # 3 perform action
+        # 4 observe new state
+        # 5 update qtable
+
+        # 1
+        currentState = grid.checkSensor()
+        actionList = self.qTable[currentState]
+        # 2
+        action = np.random.choice(
+            np.flatnonzero(np.array(actionList) == np.array(actionList).max()))
+        # action result 0 = pick up can; 1-4=>{go north, south, east, west}
         explore = np.random.choice(
-            [True, False], p=[self.epsilon, 1-self.epsilon])
+            np.array([True, False]), p=[epsilon, 1-epsilon])
+        if explore:
+            explore_action = r.randint(0, 4)
+            while explore_action == action:
+                explore_action = r.randint(0, 4)
+            action = explore_action
+        # 3
+        reward = grid.performAction(action)
+        # 4
+        nextState = grid.checkSensor()
+        # 5
+        self.qTable[currentState][action] = self.qTable[currentState][action] + self.learningRate * (reward + self.discountFactor *
+                                                                                                     max(self.qTable[nextState]) - self.qTable[currentState][action])
+        return reward
 
-        # perform action, update Q matrix
-        if not explore:
-            reward = observed_rewards[primary_move]
-            self.updateQ(board, reward, primary_move)
-            board.move(primary_move)
-            return
-
-        # perform a different action (explore)
-        explore_move = np.random.choice([0, 1, 2, 3, 4])
-        while explore_move == primary_move:
-            explore_move = np.random.choice([0, 1, 2, 3, 4])
-        reward = observed_rewards[explore_move]
-        self.updateQ(board, reward, explore_move)
-        board.move(explore_move)
-
-    def updateQ(self, board, reward, move):
-        x = board.position[0] * board.position[1]
-        y = move
-        max_Q_next = max(qTable[x])
-        self.qTable[x][y] += self.learningRate * \
-            (reward + self.discountFactor * max_Q_next - self.qTable[x][y])
-
-    # runs q learning using the values specified in the constructor to create the policy
     def train(self):
-        for run in range(self.episodes):
-            board = board()
-            for i in range(self.steps):
+        data = []
+        epsilon = 0.1
+        for episode in range(self.episodes):
+            grid = Robby()
+            reward = 0
+            if episode % 50 and episode != 0:
+                epsilon -= 0.0005
+                if epsilon < 0:
+                    epsilon = 0
+            for step in range(self.steps):
+                reward += self.greedyMove(grid, epsilon)
+            if episode % 100 == 0:
+                data.append((episode, reward))
+        plt.scatter(data[0], data[1])
+        plt.xlabel('Episode')
+        plt.ylabel('Total Reward')
+        plt.title('Training Reward')
+        plt.savefig('TrainingReward.png')
 
 
 if __name__ == '__main__':
-    print(q_learn().greedyMove())
+    QLearn().train()
